ac277f656c5c31f6eb9bb80c8e4f159325f05318
Javohir Ismatullayev <70445308+ismatullayevs@users.noreply.github.com>
2024-11-09 12:20:54 +0500

    Add refresh token rotation

--- server/auth/api.py
+++ server/auth/api.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.http import HttpResponse
 from ninja import Router
-from ninja.security import HttpBearer
+from ninja.security import APIKeyCookie
 from ninja.errors import HttpError
-
 from users.schema import UserInSchema, UserOutSchema
 from .schema import LoginSchema
 from utils import create_jwt_token
@@ -13,6 +13,13 @@ import jwt
 
 User = get_user_model()
 router = Router()
+
+
+class CookieKey(APIKeyCookie):
+    param_name = 'refresh_token'
+
+    def authenticate(self, request, key):
+        return key
 
 
 @router.post('/register', response=UserOutSchema)
@@ -25,7 +32,7 @@ def register(request, data: UserInSchema):
 
 
 @router.post('/login')
-def login(request, data: LoginSchema):
+def login(request, response: HttpResponse, data: LoginSchema):
     user = User.objects.filter(email=data.email).first()
     if user is None:
         raise HttpError(400, "Invalid email or password")
@@ -35,4 +42,32 @@ def login(request, data: LoginSchema):
 
     access_token = create_jwt_token({"sub": user.email}, type="access")
     refresh_token = create_jwt_token({"sub": user.email}, type="refresh")
-    return {"access": access_token, "refresh": refresh_token}
+
+    response.set_cookie(key="refresh_token", value=refresh_token,
+                        httponly=True, max_age=settings.REFRESH_TOKEN_EXPIRE_MINUTES * 60,
+                        samesite='strict')
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.post('/refresh', auth=CookieKey(csrf=False))
+def refresh(request, response: HttpResponse):
+    """Refresh access token using refresh token stored in cookie"""
+    print(request.auth)
+    try:
+        payload = jwt.decode(
+            request.auth, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        email: str = payload.get('sub')
+        if email is None or payload.get('type') != 'refresh':
+            raise HttpError(401, "Invalid token")
+    except jwt.PyJWTError:
+        raise HttpError(401, "Invalid token")
+
+    access_token = create_jwt_token({'sub': email}, type='access')
+    refresh_token = create_jwt_token({'sub': email}, type='refresh')
+
+    response.set_cookie(key='refresh_token', value=refresh_token,
+                        httponly=True, max_age=settings.REFRESH_TOKEN_EXPIRE_MINUTES * 60,
+                        samesite='strict')
+
+    return {"access_token": access_token, "token_type": "bearer"}
